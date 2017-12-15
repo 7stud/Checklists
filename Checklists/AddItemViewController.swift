b0542ca0fe3b099a5bd0b33c1e1e82971470d847
7stud <7stud@excite.com>
2017-12-15 08:51:39 -0700

    End of p. 274, Chapter 12. Finished Add Item View Controller.

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -2,31 +2,39 @@
 //  AddItemViewController.swift
 //  Checklists
 //
+//  Created by Christopher Wooldridge on 12/15/17.
+//  Copyright © 2017 7stud. All rights reserved.
+//
+
 import UIKit
 
 class AddItemViewController: UITableViewController {
-    @IBAction func done() {
-        navigationController?.popViewController(animated: true)
-    }
+    
+    @IBOutlet weak var textField: UITextField!
+    
     @IBAction func cancel() {
         navigationController?.popViewController(animated: true)
     }
     
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+        print("Contents of text field: \(textField.text!)")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath)
+                            -> IndexPath?
+    {
+        return nil
     }
     
-}
+    
 
+ 
+
+}
